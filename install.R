7e0b12fed5f7547bb7b8fbb3c0858521fb218f37
rronzano <remi.ronzano@ens-cachan.fr>
2022-10-20 15:49:48 +0100

    Create install.R

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages(c("openxlsx","reshape2","ggplot2","RColorBrewer","ggpubr","knitr","FSA","car","multcomp","multcompView","ggbeeswarm",
+                   "cowplot","grid", "mratios", "plotrix", "ggpval", "forcats", "plotly", "tidyr", "dplyr", "plotlyGeoAssets","tools",
+                   "stringr","png", "magick","devtools", "ggcorrplot","scales", "eulerr", "ggvenn","ggplotify","gridExtra", "boot"))
+                 
