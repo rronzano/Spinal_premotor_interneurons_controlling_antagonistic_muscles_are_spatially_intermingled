0c63687dffbc427907b206d6eee077ea3232fcad
rronzano <remi.ronzano@ens-cachan.fr>
2022-10-21 11:47:59 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages(c("openxlsx","reshape2","ggplot2","RColorBrewer","ggpubr","knitr","FSA","car","multcomp","multcompView","ggbeeswarm",
                    "cowplot","grid", "mratios", "plotrix", "ggpval", "forcats", "plotly", "tidyr", "dplyr", "plotlyGeoAssets","tools",
-                   "stringr","png", "magick","devtools", "ggcorrplot","scales", "eulerr", "ggvenn","ggplotify","gridExtra", "boot"))
+                   "stringr","png", "magick","devtools", "ggcorrplot","scales", "eulerr", "ggvenn","ggplotify","gridExtra", "boot", "markdown", "rmarkdown"))
                  
